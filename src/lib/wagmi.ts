3dfb052b3acf135d90623fa580e9a1ab3537f16a
OFUZOR CHUKWUEMEKE <uniben2018@gmail.com>
2026-04-10 10:32:08 +0100

    monad

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,12 +1,12 @@
 import { createConfig, http } from "wagmi";
-import { base, monad } from "wagmi/chains";
+import { base, monadTestnet } from "wagmi/chains";
 import { farcasterMiniApp } from "@farcaster/miniapp-wagmi-connector";
 
 export const wagmiConfig = createConfig({
-  chains: [base, monad],
+  chains: [base, monadTestnet],
   transports: {
     [base.id]: http(),
-    [monad.id]: http(),
+    [monadTestnet.id]: http(),
   },
   connectors: [farcasterMiniApp()],
 });
